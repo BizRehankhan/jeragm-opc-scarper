81bdf059810298b5ea24c35de6b555a2c0f0b17d
Rehan Khan <rehan.khan@bizmetric.com>
2026-03-30 14:59:21 +0530

    typo resolve

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ if __name__ == "__main__":
     energy_type = get_all_energy_type("Coal")
     data = single_energy_record(energy_type['_id'])
     # #  Convert Data to tabular formate
-    df = to_tabular_format(data,energy_type["_name"])
+    df = to_tabular_format(data['data'],energy_type["_name"])
     print(df.head(2))
